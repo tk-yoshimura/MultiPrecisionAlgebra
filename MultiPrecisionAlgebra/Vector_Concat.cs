e7ff8e09d8e2d237dfe0b1a41a9f29e920323ef5
tk-yoshimura <yoshimura8192@gmail.com>
2023-02-01 16:29:28 +0900

    + concat

---
+++ MultiPrecisionAlgebra/Vector_Concat.cs
@@ -0,0 +1,42 @@
+﻿using MultiPrecision;
+using System;
+using System.Collections.Generic;
+
+namespace MultiPrecisionAlgebra {
+    ///<summary>ベクトルクラス</summary>
+    public partial class Vector<N> where N : struct, IConstant {
+        /// <summary>結合</summary>
+        public static Vector<N> Concat(params object[] blocks) {
+            List<MultiPrecision<N>> v = new();
+
+            foreach (object obj in blocks) {
+                if (obj is Vector<N> vector) {
+                    v.AddRange(vector.v);
+                }
+                else if (obj is MultiPrecision<N> vmp) {
+                    v.Add(vmp);
+                }
+                else if (obj is double vd) {
+                    v.Add(vd);
+                }
+                else if (obj is int vi) {
+                    v.Add(vi);
+                }
+                else if (obj is long vl) {
+                    v.Add(vl);
+                }
+                else if (obj is float vf) {
+                    v.Add(vf);
+                }
+                else if (obj is string vs) {
+                    v.Add(vs);
+                }
+                else {
+                    throw new ArgumentException($"unsupported type '{obj.GetType().Name}'", nameof(blocks));
+                }
+            }
+
+            return new Vector<N>(v);
+        }
+    }
+}
--- MultiPrecisionAlgebraTests/VectorTests.cs
+++ MultiPrecisionAlgebraTests/VectorTests.cs
@@ -336,6 +336,26 @@ namespace MultiPrecisionAlgebra.Tests {
         }
 
         [TestMethod()]
+        public void ConcatTest() {
+            Vector<Pow2.N4> vector1 = Vector<Pow2.N4>.Fill(1, value: -1);
+            Vector<Pow2.N4> vector2 = Vector<Pow2.N4>.Fill(2, value: -2);
+            Vector<Pow2.N4> vector4 = Vector<Pow2.N4>.Fill(4, value: -3);
+
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, vector4));
+            Assert.AreEqual(new Vector<Pow2.N4>(-2, -2, -3, -3, -3, -3, -1), Vector<Pow2.N4>.Concat(vector2, vector4, vector1));
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, 1, -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, 1, vector4));
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, 2, -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, 2L, vector4));
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, 3, -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, (MultiPrecision<Pow2.N4>)3, vector4));
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, 4, -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, 4d, vector4));
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, 5, -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, 5f, vector4));
+            Assert.AreEqual(new Vector<Pow2.N4>(-1, -2, -2, "6.2", -3, -3, -3, -3), Vector<Pow2.N4>.Concat(vector1, vector2, "6.2", vector4));
+
+            Assert.ThrowsException<ArgumentException>(() => {
+                Vector<Pow2.N4>.Concat(vector1, vector2, 'b', vector4);
+            });
+        }
+
+        [TestMethod()]
         public void CopyTest() {
             Vector<Pow2.N4> vector1 = new(1, 2);
             Vector<Pow2.N4> vector2 = vector1.Copy();
