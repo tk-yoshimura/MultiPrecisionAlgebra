8e6c3d98fa888d1bbdb0c32c22d72a95250bb727
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-22 13:21:15 +0900

    cleanup

--- MultiPrecisionAlgebraTests/MatrixTests.cs
+++ MultiPrecisionAlgebraTests/MatrixTests.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MultiPrecision;
 using System;
-using System.Numerics;
 
 namespace MultiPrecisionAlgebra.Tests {
     [TestClass()]
@@ -669,7 +668,7 @@ namespace MultiPrecisionAlgebra.Tests {
 
             Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 2, 4, 8 }, { 16, 32, 64 } }), (Matrix<Pow2.N4>)(v => 2 * v, matrix1));
             Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 5, 8, 14 }, { 26, 50, 98 } }), (Matrix<Pow2.N4>)((v1, v2) => v1 + 2 * v2, (matrix1, matrix2)));
-            Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 17, 24, 38 }, { 66, 122, 234 } }),  (Matrix<Pow2.N4>)((v1, v2, v3) => v1 + 2 * v2 + 4 * v3, (matrix1, matrix2, matrix3)));
+            Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 17, 24, 38 }, { 66, 122, 234 } }), (Matrix<Pow2.N4>)((v1, v2, v3) => v1 + 2 * v2 + 4 * v3, (matrix1, matrix2, matrix3)));
             Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 49, 64, 94 }, { 154, 274, 514 } }), (Matrix<Pow2.N4>)((v1, v2, v3, v4) => v1 + 2 * v2 + 4 * v3 + 8 * v4, (matrix1, matrix2, matrix3, matrix4)));
             Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 49, 64, 94 }, { 154, 274, 514 } }), (Matrix<Pow2.N4>)((v1, v2, v3, v4) => v1 + 2 * v2 + 4 * v3 + 8 * v4, (matrix1, matrix2, matrix3, matrix4)));
 
@@ -710,14 +709,14 @@ namespace MultiPrecisionAlgebra.Tests {
             });
         }
 
-        
+
         [TestMethod()]
         public void MapTest() {
             Vector<Pow2.N4> vector1 = new(1, 2, 4, 8);
             Vector<Pow2.N4> vector2 = new(2, 3, 5);
 
             Matrix<Pow2.N4> m = ((v1, v2) => v1 + v2, vector1, vector2);
-            
+
             Assert.AreEqual(new Matrix<Pow2.N4>(new double[,] { { 3, 4, 6 }, { 4, 5, 7 }, { 6, 7, 9 }, { 10, 11, 13 } }), m);
         }
 
--- MultiPrecisionAlgebraTests/VectorTests.cs
+++ MultiPrecisionAlgebraTests/VectorTests.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MultiPrecision;
 using System;
-using System.Numerics;
 
 namespace MultiPrecisionAlgebra.Tests {
     [TestClass()]
