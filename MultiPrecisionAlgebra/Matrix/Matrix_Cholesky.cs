d30337dbd7bb599852667868199ba9e96b826dcc
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-05 23:26:51 +0900

    add: cholesky

---
+++ MultiPrecisionAlgebra/Matrix/Matrix_Cholesky.cs
@@ -0,0 +1,54 @@
+﻿using MultiPrecision;
+using System;
+
+namespace MultiPrecisionAlgebra {
+    /// <summary>行列クラス</summary>
+    public partial class Matrix<N> where N : struct, IConstant {
+        /// <summary>コレスキー分解</summary>
+        public static Matrix<N> Cholesky(Matrix<N> m, bool enable_check_symmetric = true) {
+            if (!IsSquare(m)) {
+                throw new ArgumentException("not square matrix", nameof(m));
+            }
+
+            int n = m.Size;
+            
+            if ((enable_check_symmetric && !IsSymmetric(m)) || !IsFinite(m)) {
+                return Invalid(n);
+            }
+
+            if (IsZero(m)) {
+                return Zero(n);
+            }
+
+            long exponent = (m.MaxExponent / 2) * 2;
+
+            Matrix<N> u = ScaleB(m, -exponent);
+
+            MultiPrecision<N>[,] v = new MultiPrecision<N>[n, n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    MultiPrecision<N> v_ij = u[i, j];
+                    for (int k = 0; k < j; k++) {
+                        v_ij -= v[i, k] * v[j, k];
+                    }
+                    v[i, j] = v_ij / v[j, j]; 
+                }
+
+                MultiPrecision<N> v_ii = u[i, i];
+                for (int k = 0; k < i; k++) {
+                    v_ii -= v[i, k] * v[i, k];
+                }
+                v[i, i] = MultiPrecision<N>.Sqrt(v_ii);
+
+                for (int j = i + 1; j < n; j++) {
+                    v[i, j] = MultiPrecision<N>.Zero;
+                }
+            }
+
+            Matrix<N> l = ScaleB(new Matrix<N>(v, cloning: false), exponent / 2);
+
+            return l;
+        }
+    }
+}
---
+++ MultiPrecisionAlgebraTests/Matrix/MatrixTests_Cholesky.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiPrecision;
+using MultiPrecisionAlgebra;
+using System;
+
+namespace MultiPrecisionAlgebraTests {
+    public partial class MatrixTests {
+        [TestMethod()]
+        public void CholeskyTest() {
+            foreach (Matrix<Pow2.N4> matrix in MatrixTestCases<Pow2.N4>.PositiveMatrixs) {
+                Console.WriteLine($"test: {matrix}");
+
+                Matrix<Pow2.N4> l = Matrix<Pow2.N4>.Cholesky(matrix);
+                Matrix<Pow2.N4> v = l * l.T;
+
+                Assert.IsTrue((matrix - v).Norm < 1e-32);
+            }
+        }
+    }
+}
